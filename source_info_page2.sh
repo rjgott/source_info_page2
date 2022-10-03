bf40cda33ac53620977a874c9c0cf7559009419a
rjgott <rgott@ufl.edu>
2022-10-03 09:59:59 -0400

    Title variable assigned source_info_page2.sh

--- source_info_page2.sh
+++ source_info_page2.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
  # Program to output a system information page
+
+title="Source Information Page"
+
 echo "<html>"
 echo "   <head>"
-echo "       <title>Source Information Report</title>"
+echo "       <title>$title</title>"
 echo "   </head>"
 echo "   <body>"
-echo "       Source Information Report."
+echo "       $title."
 echo "	</body>"
 echo "</html>"
